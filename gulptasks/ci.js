cfaf4ee5575429fae6ef2b6d9e39aff7879040b4
Scott Lyons <scott.lyons@advancedcomputersoftware.com>
2017-03-29 14:53:49 +0100

    Removed unused files

--- gulptasks/ci.js
+++
@@ -1,6 +0,0 @@
-var gulp = require('gulp'),
-    runSequence = require('run-sequence');
-    
-gulp.task('ci', function(done) {
-    runSequence('build', 'copy-js', 'copy-html', 'compress', done);
-});
--- gulptasks/postbuild.js
+++
@@ -1,6 +0,0 @@
-var gulp = require('gulp'),
-    runSequence = require('run-sequence');
-    
-gulp.task('postbuild', function(done) {
-    runSequence('ts-compile', 'copy-files', done);
-});
